4917ba72ad9ad1a437a91e9b89b3c6117954c336
dev-paprika <paprika0910@gmail.com>
2024-07-17 21:51:15 +0900

    first commit

---
+++ src/main/java/management/student/ManagementStudent/ServletInitializer.java
@@ -0,0 +1,13 @@
+package management.student.ManagementStudent;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ManagementStudentApplication.class);
+	}
+
+}
---
+++ src/test/java/management/student/ManagementStudent/ManagementStudentApplicationTests.java
@@ -0,0 +1,13 @@
+package management.student.ManagementStudent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementStudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
