5570cc817034878213df1a5d5d5e2a23b16e6e2a
dev-paprika <paprika0910@gmail.com>
2024-07-20 18:04:58 +0900

    不要なメソッドを削除

--- src/main/java/management/student/ManagementStudent/StudentRepository.java
+++ src/main/java/management/student/ManagementStudent/StudentRepository.java
@@ -1,11 +1,7 @@
 package management.student.ManagementStudent;
 
-import java.util.List;
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface StudentRepository {
@@ -13,18 +9,5 @@ public interface StudentRepository {
   @Select("SELECT * FROM student WHERE name = #{name}")
     //引数のnameと#のnameが紐づいている
   Student searchByName(String name);
-
-  @Select("SELECT * FROM student")
-  List<Student> searchAllStudent();
-
-  @Insert("INSERT student VALUES(#{name},#{age})")
-  void registerStudent(String name, int age);
-
-  @Update("UPDATE student SET age = #{age} where name = #{name}")
-  void updateStudent(String name, int age);
-
-  @Delete("DELETE FROM student WHERE name = #{name}")
-  void deleteStudent(String name);
-
 
 }
